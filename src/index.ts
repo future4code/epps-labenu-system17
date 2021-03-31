f76a952ffcb36a5c2626d1df8e10ec5428f64bab
Allison Araujo <allisonaraujo94@outlook.com>
2021-03-31 20:06:38 -0300

    Não mergear

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,26 @@
-import express, {Express} from 'express'
+import express, { Express } from 'express'
 import cors from 'cors'
-import { AddressInfo } from "net";
-import knex from "knex";
-import dotenv from "dotenv";
+// import knex from "knex";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  }
-});
+// export const connection = knex({
+// 	client: "mysql",
+// 	connection: {
+//     host: process.env.DB_HOST,
+//     port: 3306,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+//   }
+// });
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    }
-});
+app.listen(3003, () => {
+   console.log(`Server is running);
+}
