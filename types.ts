5736ec0e06af494aa03d1d04a49b5e5ed65a1d63
Vivian-Oliveira <costa.vivs@gmail.com>
2021-03-31 16:03:30 +0100

    finalizei estrutura do projeto

---
+++ types.ts
@@ -0,0 +1,39 @@
+export type students = {
+    id: number,
+    nome: string,
+    email: string,
+    data_nasc: Date,
+    turma_id: number
+};
+
+export type hobby = {
+    id: number,
+    nome: string
+}
+
+export type students_hobby = {
+    estudantes_id: number,
+    passatempo_id: number
+}
+
+export type Class = {
+    id: number,
+    nome: string,
+    data_inicio: Date,
+    data_final: Date,
+    modulo: number
+};
+
+export type Teacher = {
+    id: number,
+    nome: string,
+    email: string,
+    data_nasc: Date,
+    turma_id: number
+};
+
+export type teacher_expertise = {
+    docente_id: number,
+    especialidade_id: number
+};
+
