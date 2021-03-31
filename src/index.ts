9059bf0b7c63f317b63a7549aecbfaa907f391d2
Vivian-Oliveira <costa.vivs@gmail.com>
2021-03-31 16:30:54 +0100

    finalizei estrutura do projeto

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import app from "../app"
+import app from "./app"
 import addStudent from "./endpoints/addStudent"
 import addTeacher from "./endpoints/addTeacher"
 import createStudent from "./endpoints/createStudent"
