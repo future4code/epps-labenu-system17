915c7fb9bfd04876f18c602e8991c548ac68e139
Allison Araujo <allisonaraujo94@outlook.com>
2021-04-02 17:00:52 -0300

    Create Teacher

--- src/endpoints/createTeacher.ts
+++ src/endpoints/createTeacher.ts
@@ -4,32 +4,6 @@ import insertTeacher from "../data/insertTeacher"
 import { expertise, Teacher } from "../types"
 
 
-// export default async function (
-//    req: Request,
-//    res: Response
-// ): Promise<void> {
-
-//    try {
-//       if(
-//          !req.body.nome ||
-//          !req.body.email ||
-//          !req.body.data_nasc ||
-//          !req.body.turma_id
-//       )
-//       res.status(400).send("Campos obrigatorios")
-
-
-//       await insertTeacher(
-//          req.body.nome,
-//          req.body.email,
-//          req.body.data_nasc,
-//          req.body.turma_id
-//       )
-//       res.status(200).send("Professor criado com sucesso!")
-
-
-
-
 export default async function createTeacher(
    req: Request,
    res: Response
