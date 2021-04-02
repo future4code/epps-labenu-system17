2d983e7386cf2fcc9460632fb93cad263c772659
Vivian-Oliveira <costa.vivs@gmail.com>
2021-04-03 00:45:04 +0100

    finalizamos endpoints docente

--- src/endpoints/createTeacher.ts
+++ src/endpoints/createTeacher.ts
@@ -12,19 +12,6 @@ export default async function createTeacher(
          data_nasc: req.body.data_nasc,
          turma_id: req.body.turma_id
       }
-      // const especialidades: expertise = {
-      //    especialidade: req.body.especialidade
-      // }
-
-      // if ( !req.body.especialidade) {
-      //    res.status(400).send('Invalid Parameters test.')
-      // } else {
-      //    await connection.raw(`
-      //       INSERT INTO expertise (especialidade) VALUES (
-      //          "${req.body.especialidade}"
-      //       )
-      //    `)
-      // }
       if (!req.body.nome || !req.body.email || !req.body.data_nasc || !req.body.turma_id) {
          res.status(400).send('Invalid Parameters.')
       } else {
@@ -52,35 +39,3 @@ export default async function createTeacher(
 }
 
 
-// import { Request, Response } from "express"
-// import insertTeacher from "../data/insertTeacher"
-
-
-// export default async function createTeacher(
-//    req: Request,
-//    res: Response
-// ): Promise<void> {
-
-//    try {
-//       if(
-//          !req.body.nome ||
-//          !req.body.email ||
-//          !req.body.data_nasc ||
-//          !req.body.turma_id
-//       )
-//       res.status(400).send("Campos obrigatorios")
-
-
-//       await insertTeacher(
-//          req.body.nome,
-//          req.body.email,
-//          req.body.data_nasc,
-//          req.body.turma_id
-//       )
-//       res.status(200).send("Professor criado com sucesso!")
-
-
-//    } catch (error) {
-//       res.status(500).send({message: error.message || error.sqlMessage})
-//    }
-// }
